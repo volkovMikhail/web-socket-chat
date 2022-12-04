ff39a8ddf052ebe341e64d683faa3f825214d0da
volkovMikhail <volkov.work1@gmail.com>
2022-12-04 18:39:37 +0100

    add image uploading

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ console.log(username, sessionID);
 const textArea = document.querySelector('#text');
 const sendBtn = document.querySelector('#send');
 const chat = document.querySelector('#chat');
+const fileInput = document.querySelector('#file');
 const userSpan = (document.querySelector('#username').innerHTML += username);
 
 //web-socket
@@ -12,84 +13,111 @@ const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
 let socket;
 
 function startSocket() {
-    socket = new WebSocket(`${protocol}://${window.location.host}/`);
+  socket = new WebSocket(`${protocol}://${window.location.host}/`);
 
-    socket.onmessage = (event) => {
-        chat.insertAdjacentHTML(
-            'afterbegin',
-            `
-            <div class="message">${event.data}</div>
-            `
-        );
-    };
+  socket.onmessage = (event) => {
+    const msg = JSON.parse(event.data);
+    showMassage(msg);
+  };
 
-    socket.onclose = () => {
-        startSocket();
-        console.log('reconnect');
-    };
+  socket.onclose = () => {
+    startSocket();
+    console.log('reconnect');
+  };
 }
 
 startSocket();
 
 socket.onopen = () => {
-    socket.send(
-        JSON.stringify({
-            id: sessionID,
-            username: username,
-            message: '',
-            method: 'connection',
-        })
-    );
-    fetchData();
+  socket.send(
+    JSON.stringify({
+      id: sessionID,
+      username: username,
+      message: '',
+      method: 'connection',
+    })
+  );
+  fetchData();
 };
 
 //recive all data
 async function fetchData() {
-    const data = await (await fetch('/messages')).json();
-    data.forEach((msg) => {
-        switch (msg.method) {
-            case 'connection':
-                chat.insertAdjacentHTML(
-                    'afterbegin',
-                    `
-                    <div class="message">${msg.username} has joined chat!</div>
-                    `
-                );
-                break;
-
-            default:
-                chat.insertAdjacentHTML(
-                    'afterbegin',
-                    `
-                    <div class="message">${msg.username}: ${msg.message}</div>
-                    `
-                );
-                break;
-        }
-    });
+  const data = await (await fetch('/messages')).json();
+  data.forEach((msg) => {
+    showMassage(msg);
+  });
 }
 
 //client events
 window.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        sendMessage();
-    }
+  if (e.key === 'Enter') {
+    sendMessage();
+  }
 });
 
 sendBtn.addEventListener('click', () => {
-    sendMessage();
+  sendMessage();
+});
+
+fileInput.addEventListener('change', async (e) => {
+  const file = e.target.files[0];
+
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const res = await (await fetch('/upload', { method: 'POST', body: formData })).json();
+
+  const msg = {
+    id: sessionID,
+    username: username,
+    message: res.filename,
+    method: 'file',
+  };
+
+  socket.send(JSON.stringify(msg));
+  console.log('file send');
+  showMassage(msg);
+  e.target.value = '';
 });
 
 //functions
 function sendMessage() {
-    socket.send(
-        JSON.stringify({
-            id: sessionID,
-            username: username,
-            message: textArea.value,
-            method: 'send',
-        })
-    );
-    textArea.value = '';
-    console.log('message send');
+  if (!textArea.value.trim()) {
+    return;
+  }
+
+  socket.send(
+    JSON.stringify({
+      id: sessionID,
+      username: username,
+      message: textArea.value,
+      method: 'send',
+    })
+  );
+  textArea.value = '';
+  console.log('message send');
+}
+
+function showMassage(msg) {
+  switch (msg.method) {
+    case 'connection':
+      chat.insertAdjacentHTML('afterbegin', `<div class="message">${msg.username} has joined chat!</div>`);
+      break;
+    case 'send':
+      chat.insertAdjacentHTML('afterbegin', `<div class="message">${msg.username}: ${msg.message}</div>`);
+      break;
+    case 'file':
+      chat.insertAdjacentHTML(
+        'afterbegin',
+        `
+            <div class="message">
+                ${msg.username} sent file:
+            </div>
+            <img class="message" src="/${msg.message}" alt="/${msg.message}" />
+        `
+      );
+      break;
+    default:
+      break;
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,60 +1,73 @@
 const path = require('path');
 const express = require('express');
+const multer = require('multer');
 const app = express();
 const WSServer = require('express-ws')(app);
 const aWss = WSServer.getWss();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, '..', 'client'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const PORT = process.env.PORT || 5000;
 const DATABASE = [];
 
 app.use(express.static(path.resolve('client')));
 
-app.ws('/',(ws,req)=>{
-    console.log('connection is OK');    
-    ws.on('message',(msg)=>{
-        const message = JSON.parse(msg);
-        switch (message.method) {
-            case 'connection':
-                saveToDB(message);
-                connectionHandler(ws,message);
-                break;
-        
-            case 'send':
-                saveToDB(message);
-                broadcastConnection(ws,message,`${message.username}: ${message.message}`)
-                break;
-            default:
-                
-                break;
-        }
-    })
-})
+app.ws('/', (ws, req) => {
+  console.log('connection is OK');
+  ws.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.method) {
+      case 'connection':
+        saveToDB(message);
+        connectionHandler(ws, message);
+        break;
+      default:
+        saveToDB(message);
+        console.log(message);
+        broadcastConnection(ws, message, JSON.stringify(message));
+        break;
+    }
+  });
+});
 
-app.get('/messages',(req,res)=>{
-    res.json(DATABASE);
-})
+app.post('/upload', upload.single('file'), function (req, res) {
+  return res.json({ filename: req.file.filename });
+});
+
+app.get('/messages', (req, res) => {
+  res.json(DATABASE);
+});
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'index.html'));
+  res.sendFile(path.resolve('client', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`server started on port: ${PORT}...`);
+  console.log(`server started on port: ${PORT}...`);
 });
 
-function connectionHandler(ws,msg){
-    ws.id = msg.id;
-    broadcastConnection(ws,msg,`${msg.username} has joined chat!`);
+function connectionHandler(ws, msg) {
+  ws.id = msg.id;
+  broadcastConnection(ws, msg, JSON.stringify(msg));
 }
 
-function broadcastConnection(ws,msg,alert){
-    aWss.clients.forEach(client=>{
-        if (client.id !== msg.id || msg.method ==='send') {
-            client.send(alert);
-        }
-    })
+function broadcastConnection(ws, msg, alert) {
+  aWss.clients.forEach((client) => {
+    if (client.id !== msg.id || msg.method === 'send') {
+      client.send(alert);
+    }
+  });
 }
 
-function saveToDB(message){
-    DATABASE.push(message);
+function saveToDB(message) {
+  DATABASE.push(message);
 }
